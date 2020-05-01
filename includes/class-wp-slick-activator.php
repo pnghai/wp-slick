591cecadc629ae1bc1fbba69b35ef549b5535112
hai <pnghai@gmail.com>
2020-05-01 23:32:41 +0700

    initial sourcecode

---
+++ includes/class-wp-slick-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://github.com/pnghai
+ * @since      1.0.0
+ *
+ * @package    Wp_Slick
+ * @subpackage Wp_Slick/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Slick
+ * @subpackage Wp_Slick/includes
+ * @author     pnghai <pnghai@gmail.com>
+ */
+class Wp_Slick_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
